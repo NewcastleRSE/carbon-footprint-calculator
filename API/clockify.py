1de144d9291c59235790ff2527ba28f7dd778e42
OEresanmi <C40457945@newastle.ncl.ac.uk>
2025-04-10 10:21:16 +0100

    - Comment added to improve readability of code - Changes the variable mylist to timesList

--- API/clockify.py
+++ API/clockify.py
@@ -8,29 +8,49 @@ headers = {
     'X-Api-Key': API_KEY
 }
 
+"""
+stringSplit: This function is meant to split the given string
+into a list of strings removing all invalid letters. 
+
+sample input - "PT123H456M78S"
+sample output - ["123", "456", "78"]
+"""
 def stringSplit(duration):
-    translation_table = str.maketrans("PTHMS", "     ")
+    # The translation table replaces all characters in a given string with spaces
+    translation_table = str.maketrans("PTHMS", "     ") # (old_str, new_str)
     translated_string = duration.translate(translation_table)
     times = translated_string.strip().split(" ")
 
     return times
 
+
+"""
+calcEfficiency: This function is meant to calculate the average wattage per hour
+on all projects. 
+
+sample input - (a dict of projects with attributes of duration and memberships, 
+                a dict of userId's and their corresponding computer wattage
+sample output - null - prints results to terminal
+"""
 def calcEfficiency(projects, data):
      for project in projects:
+         # retrieve the duration of a project - "PT123H456M78S"
         duration = project["duration"]
-        mylist = stringSplit(duration)
-        if len(mylist) == 3:
-            hour = int(mylist[0]) * 60
-            min = int(mylist[1])
-            sec = int(mylist[2]) / 60
+        timesList = stringSplit(duration)
+        # Convert each item of the list into the total minutes of each project
+        if len(timesList) == 3:
+            hour = int(timesList[0]) * 60
+            min = int(timesList[1])
+            sec = int(timesList[2]) / 60
             total = hour + min + sec
-        elif len(mylist) == 2:
-            min = int(mylist[0])
-            sec = int(mylist[1]) / 60
+        elif len(timesList) == 2:
+            min = int(timesList[0])
+            sec = int(timesList[1]) / 60
             total = min + sec
         else:
-            total = int(mylist[0]) / 60
+            total = int(timesList[0]) / 60
 
+        # calculate the avg wattage per project based on data (specs.json)
         wattage = 0
         users = project["memberships"]
         for user in users:
